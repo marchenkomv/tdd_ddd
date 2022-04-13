5ac2563ca6f033fe8e7364821dcab3dcaa90fa62
marchenkomv <marchenko_m.v@mail.ru>
2022-04-13 17:20:06 +1000

    add methods realization

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -3,14 +3,15 @@ public class CreditCalculator {
     private final int MONTH_IN_YEAR = 12;
 
     public double calcMonthPayment(double amount, double rate, int monthCount) {
-        return 0.0;
+        return calcTotalAmount(amount, rate, monthCount) / monthCount;
     }
 
     public double calcTotalAmount(double amount, double rate, int monthCount) {
-        return 0.0;
+        return amount + calcRevenueAmount(amount, rate, monthCount);
     }
 
     public double calcRevenueAmount(double amount, double rate, int monthCount) {
-        return 0.0;
+        double monthRevenue = amount * rate / MONTH_IN_YEAR;
+        return monthRevenue * monthCount;
     }
 }
